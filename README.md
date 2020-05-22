e06dd4468d99874cf5847c96892e523c01ef02a5
Keith-Njagi <keithgatana@gmail.com>
2020-05-22 12:33:22 +0300

    app structure laid out

---
+++ README.md
@@ -0,0 +1,3 @@
+# USER MANAGEMENT BACKEND
+
+This service is created to serve as the backend authentication/authorization in the main project.
---
+++ app/configurations/__init__.py
@@ -0,0 +1,74 @@
+import os 
+
+class Config(object):
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
+    ENVIRONMENT = 'Development'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    PROPAGATE_EXCEPTIONS = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+class Development(Config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = False
+    ENVIRONMENT = 'Development'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    PROPAGATE_EXCEPTIONS = True
+    SQLALCHEMY_ENGINE_OPTIONS = os.getenv('SQLALCHEMY_ENGINE_OPTIONS')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_BLACKLIST_ENABLED = True  # enable blacklist feature
+    JWT_BLACKLIST_TOKEN_CHECKS = os.getenv('JWT_BLACKLIST_TOKEN_CHECKS')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = os.getenv('MAIL_SERVER'),
+    MAIL_PORT = os.getenv('JWT_SECRET_KEY'),
+    MAIL_USE_TLS = os.getenv('MAIL_PORT'),
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL'),
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_ASCII_ATTACHMENTS = os.getenv('MAIL_ASCII_ATTACHMENTS')
+
+class Testing (Config):
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
+    ENVIRONMENT = 'Development'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    PROPAGATE_EXCEPTIONS = True
+    SQLALCHEMY_ENGINE_OPTIONS = os.getenv('SQLALCHEMY_ENGINE_OPTIONS')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_BLACKLIST_ENABLED = True  # enable blacklist feature
+    JWT_BLACKLIST_TOKEN_CHECKS = os.getenv('JWT_BLACKLIST_TOKEN_CHECKS')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = os.getenv('MAIL_SERVER'),
+    MAIL_PORT = os.getenv('JWT_SECRET_KEY'),
+    MAIL_USE_TLS = os.getenv('MAIL_PORT'),
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL'),
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_ASCII_ATTACHMENTS = os.getenv('MAIL_ASCII_ATTACHMENTS')
+
+class Production(Config):
+    DEBUG = False
+    SQLALCHEMY_ECHO = False
+    ENVIRONMENT = 'Production'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    PROPAGATE_EXCEPTIONS = True
+    SQLALCHEMY_ENGINE_OPTIONS = os.getenv('SQLALCHEMY_ENGINE_OPTIONS')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_BLACKLIST_ENABLED = True  # enable blacklist feature
+    JWT_BLACKLIST_TOKEN_CHECKS = os.getenv('JWT_BLACKLIST_TOKEN_CHECKS')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = os.getenv('MAIL_SERVER'),
+    MAIL_PORT = os.getenv('JWT_SECRET_KEY'),
+    MAIL_USE_TLS = os.getenv('MAIL_PORT'),
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL'),
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_ASCII_ATTACHMENTS = os.getenv('MAIL_ASCII_ATTACHMENTS')
